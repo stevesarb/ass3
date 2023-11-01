9c72677117c1815c545843fdb071cba9c8a763bc
stevesarb <sesarber@gmail.com>
2023-11-01 16:56:28 -0700

    displays prompt, exits appropriately

--- code/main.c
+++ code/main.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int DEBUGMODE = 0;
 
 
 int main() {
+	char* line = NULL;
+    size_t len = 0;
+    ssize_t lineSize = 0;
 	
-	do {
-		printf("\n: ");
-		// add getline
+	while (1) {
+		printf(": ");
+		lineSize = getline(&line, &len, stdin);
+		if (DEBUGMODE)
+			printf("\nlen = %d ; linesize = %d\n", len, lineSize);
+
+		if ((strncmp(line, "exit", 4) == 0) && (lineSize == 5))
+			break;
+		free(line);
+		line = NULL;
 	}
-	while (1);
 
 	return 0;
 }
